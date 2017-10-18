40f9a70fe6117a5485556cc0d91e82a7be8c2095
VitozzzZ <zealucius@gmail.com>
2017-10-18 17:35:10 +0800

    Delete SCHEMA.sql

--- Create/SCHEMA.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA TEST AUTHORIZATION ZHANG
-CREATE TABLE TAB1(COL1 SMALLINT,
-                  COL2 INT,
-                  COL3 CHAR(20),
-                  COL4 NUMERIC(10,3),
-                  COL5 DECIMAL(5,2)
-                  );
