22c3b6088c63bb133985b2c098e5f9be0358dfea
VitozzzZ <zealucius@gmail.com>
2017-10-18 17:21:56 +0800

    Update SCHEMA.sql

--- CreateAndDrop/SCHEMA.sql
+++ CreateAndDrop/SCHEMA.sql
@@ -1,7 +1,7 @@
-//CERATE SCHEMA<模式名>AUTHORIZATION<用户名>
-//DROP SCHEMA<模式名><CASCADE|RESTRICT>
+/*CERATE SCHEMA<模式名>AUTHORIZATION<用户名>
+DROP SCHEMA<模式名><CASCADE|RESTRICT>*/
 
 CREATE SCHEMA S-T AUTHORIZATION WANG
 DROP SCHEMA S-T CASCADE
 
-//CASCADE(级联)：删除该模式中所有数据库对象 RESRICT(限制)：若该模式没有任何下属数据库对象才执行删除
+/*CASCADE(级联)：删除该模式中所有数据库对象 RESRICT(限制)：若该模式没有任何下属数据库对象才执行删除*/
