8e1933a82877eac254fda6beb27eaca22a6f8f46
VitozzzZ <zealucius@gmail.com>
2017-11-01 16:46:05 +0800

    Update NestedQuery.sql

--- Query/NestedQuery.sql
+++ Query/NestedQuery.sql
@@ -11,7 +11,7 @@
 /*查询与“刘晨”在同一个系学习的学生*/
 SELECT Sno,Sname,Sdept
 FROM Student
-WHERE Sdept IN
+WHERE Sdept IN  /*若一个学生只可能在一个系，可用=代替IN*/
      (SELECT Sdept
       FROM Student
       WHERE Sname='刘晨');
@@ -35,3 +35,31 @@ WHERE Sno IN
 SELECT Student.Sno,Sname  /*解法2*/
 FROM Student,SC,Course
 WHERE Student.Sno=SC.Sno AND SC.Cno=Course.Cno AND Course.Cname='信息系统';
+
+/*2.带有比较运算符的子查询*/
+
+/*找出每个学生超过他自己选修课程平均成绩的课程号*/
+SELECT Sno,Cno
+FROM SC x     /*元组变量，可以用来表示SC表的一个元组*/
+WHERE Grade>=(SELECT AVG(Grade)
+              FROM SC y
+              WHERE y.Sno=x.Sno);/*相关子查询*/
+
+/*3.带有ANY(SOME)或ALL谓词的子查询 >ANY大于子查询结果中的某个值 >ALL大于子查询中的所有值*/
+/*查询非计算机科学系中比计算机科学系任意一个学生年龄小的学生姓名和年龄*/
+SELECT Sname,Sage
+FROM Student
+WHERE Sage<ANY(SELECT Sage
+               FROM Student
+               WHERE Sdept='CS')
+AND Sdept<>'CS';
+
+SELECT Sname,Sage
+FROM Student
+WHERE Sage<(SELECT MAX(Sage) /*解法2*/
+            FROM Student
+            WHERE Sdept='CS')
+AND Sdept<>'CS';
+
+/*4.带有EXISTS谓词的子查询*/
+              
