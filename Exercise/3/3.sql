da96b2917162f9492caf3cdddab05231afc431b1
VitozzzZ <zealucius@gmail.com>
2017-12-22 17:16:12 +0800

    Update 3.sql

--- Exercise/3/3.sql
+++ Exercise/3/3.sql
@@ -1,4 +1,5 @@
 /*
+1.  计算出一共销售了几种商品，输出商品数量
 2.	在订单明细表中查询订单金额最高的订单，输出订单编号和订单金额。
 3.	显示OrderDetail表中每种商品和对应的销售金额，并且按由大到小排序输出。
 4.	查找销售总额大于1000元的销售员编号、姓名和销售额。
@@ -16,4 +17,60 @@
 16.	求每种商品的总销售数量及总销售金额，要求显示出商品编号、商品名称、总数
 */
 
+--1
+
+SELECT COUNT(DISTINCT productNo )
+FROM OrderDetail
+
 --2
+SELECT TOP 1 orderNo,SUM(quantity*price) MONEY
+FROM OrderDetail
+GROUP BY orderNo
+ORDER BY MONEY DESC
+
+--3
+
+SELECT productNo,SUM(quantity*price) MONEY
+FROM OrderDetail
+GROUP BY productNo
+ORDER BY MONEY DESC
+
+--4
+
+SELECT Employee.employeeNo,Employee.employeeName,B.MONEY
+FROM(SELECT salerNo,SUM(money) MONEY
+FROM OrderMaster,(SELECT orderNo,SUM(price*quantity) money
+FROM OrderDetail
+GROUP BY orderNo) AS A
+WHERE A.orderNo=OrderMaster.orderNo AND MONEY>1000
+GROUP BY salerNo) AS B,Employee
+WHERE B.salerNo=Employee.employeeNo
+
+--5
+SELECT customerName,Customer.customerNo,A.MONEY
+FROM Customer,(SELECT customerNo,SUM(quantity*price) MONEY
+FROM OrderMaster,OrderDetail
+WHERE OrderMaster.orderNo=OrderDetail.orderNo 
+GROUP BY customerNo) AS A
+WHERE Customer.customerNo=A.customerNo
+
+--6
+SELECT customerNo,customerName
+FROM Customer
+WHERE Customer.customerNo NOT IN (SELECT B.customerNo
+FROM(SELECT customerName,Customer.customerNo,A.MONEY
+FROM Customer,(SELECT customerNo,SUM(quantity*price) MONEY
+FROM OrderMaster,OrderDetail
+WHERE OrderMaster.orderNo=OrderDetail.orderNo 
+GROUP BY customerNo) AS A
+WHERE Customer.customerNo=A.customerNo) AS B)
+
+--7
+SELECT productNo,orderNo,quantity,quantity*price 订货金额
+FROM OrderDetail
+WHERE productNo IN(
+SELECT productNo
+FROM OrderDetail
+GROUP BY productNo
+HAVING COUNT(productNo)>=3)
+ORDER BY productNo,quantity
